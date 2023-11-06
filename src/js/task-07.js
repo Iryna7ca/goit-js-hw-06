f42364167768875c31b08faec508259293ba5dfb
Iryna7ca <gorpinichvorona@gmail.com>
2023-11-06 16:15:13 +0100

    task-07

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,1 +1,12 @@
 const controlLine = document.querySelector("#font-size-control");
+ 
+const text = document.querySelector("#text");
+ 
+ 
+controlLine.addEventListener("input", onInputChange);
+function onInputChange(event) {
+     
+     const textFontSize = parseInt(event.currentTarget.value);
+    text.style.fontSize = `${textFontSize}px`;
+};
+ 
