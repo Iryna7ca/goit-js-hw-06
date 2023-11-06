fb9cdef9768f50b32e16a4014c5d955986b58a10
Iryna7ca <gorpinichvorona@gmail.com>
2023-11-06 18:01:47 +0100

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
+ 
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (form.email.value === `` || form.password.value === ``) {
+        alert("Всі поля мають бути заповнені!");
+    } else {
+        console.dir(`Email: ${form.email.value}, Password: ${form.password.value}`);
+        form.reset();
+    }
+    
+     
+};
