dd34163c44c64ec7efa7c5e2fca8e7122b964d48
Iryna7ca <gorpinichvorona@gmail.com>
2023-11-09 22:43:01 +0100

    chanched3

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll('.item');
-console.log("Number of categories:", items.length);
-
-items.forEach((item) => {
-    const nameCategory = item.querySelector("h2").textContent;
-    const counter = item.querySelectorAll("li").length;
-    console.log(`Category: ${nameCategory}`);
-    console.log(`Elements: ${counter}`);
-});
