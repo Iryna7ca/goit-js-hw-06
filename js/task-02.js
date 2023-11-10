525c0beda1589ddb49c29d6b74eb76b3b1709467
Iryna7ca <gorpinichvorona@gmail.com>
2023-11-10 18:35:56 +0100

    update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -8,18 +8,29 @@ const ingredients = [
 ];
 
 const list = document.querySelector("#ingredients");
- 
-// ingredients.forEach((ingredient) => {
-//   const listItem = document.createElement("li");
+
+function createMarcup(ingredients) {
+  const markup = ingredients.map((ingredient) => {
+    const li = document.createElement("li");
+    li.textContent = `${ingredient}`;
+    li.classList.add("item");
+    list.append(li);
+    return li;
+  })
+   
+}
+ createMarcup(ingredients)
+//  ingredients.forEach((ingredient) => {
+//    const listItem = document.createElement("li");
 //   listItem.textContent =  `${ingredient}`;
-//   listItem.classList.add("item");
+//    listItem.classList.add("item");
 //   list.append(listItem);
 
-// });
+//  });
 
-const markup = ingredients
-  .map((ingredient) => `<li class="item">${ingredient}</li>`)
-  .join("");
-  list.insertAdjacentHTML("afterbegin", markup);
+// const markup = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join("");
+//   list.insertAdjacentHTML("afterbegin", markup);
 
  
